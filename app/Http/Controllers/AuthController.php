5df8b4f96ed174f321f990ec0230636686c7876d
Hai Tran <hai.ts.2166@aptechlearning.edu.vn>
2023-07-28 01:16:47 +0700

    Temporary duplicated assets into resources and public

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Show the login form.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Handle user login attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed, user is logged in.
+            return redirect()->intended('/dashboard'); // Redirect to the dashboard or any other desired route upon successful login.
+        }
+
+        // Authentication failed, redirect back to the login form with errors.
+        return redirect()->back()->withInput()->withErrors([
+            'email' => 'Invalid credentials.',
+        ]);
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Login Routes
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+// Logout Route
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
