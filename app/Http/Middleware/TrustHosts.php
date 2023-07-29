754c682cddc31fcc6343d34157f8d1d281887fd6
Hai Tran <hai.ts.2166@aptechlearning.edu.vn>
2023-07-30 04:18:31 +0700

    Add heroku url as trusted host

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -14,6 +14,7 @@ class TrustHosts extends Middleware
     public function hosts(): array
     {
         return [
+            'bronx-luggage-1fa1b84e54da.herokuapp.com',
             $this->allSubdomainsOfApplicationUrl(),
         ];
     }
