701315d52e2596ae8f9ae2d079cc435c6b0afe3c
Hai Tran <hai.ts.2166@aptechlearning.edu.vn>
2023-08-27 13:03:42 +0700

    Add sidebar for dashboard admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,11 +3,12 @@
 
 <body>
   <div id="app">
-    <h1>Admin</h1>
-
-    <div class="container mb-5">
-      @include('shared._flash_message')
-      @yield('content')
-    </div>
+    <main class="d-flex flex-nowrap">
+      @include('layouts.admin._sidebar')
+      <div class="container mb-5">
+        @include('shared._flash_message')
+        @yield('content')
+      </div>
+    </main>
   </div>
 </body>
