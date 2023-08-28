05314758575f56215785c2d63bbc80ded0a79d24
Hai Tran <hai.ts.2166@aptechlearning.edu.vn>
2023-08-28 09:52:04 +0700

    fix indentation

--- resources/views/admin/carriers/index.blade.php
+++ resources/views/admin/carriers/index.blade.php
@@ -10,15 +10,16 @@
 <div class="pagination">
   <ul>
     @if ($paginator['last_page'] > 1)
-    @for($i = 0; $i < $paginator['last_page']; $i++) <li class="{{$paginator['page'] == $i + 1 ? 'current' : ''}}">
-      <a href="{{route('admin-carriers', ['page' => $i + 1])}}">{{$i + 1}}</a>
-      </li>
+      @for($i = 0; $i < $paginator['last_page']; $i++)
+        <li class="{{$paginator['page'] == $i + 1 ? 'current' : ''}}">
+          <a href="{{route('admin-carriers', ['page' => $i + 1])}}">{{$i + 1}}</a>
+        </li>
       @endfor
 
       @if ($paginator['has_more_page'])
-      <li class="next"><a href="{{$paginator['next']}}">next</a></li>
+        <li class="next"><a href="{{$paginator['next']}}">next</a></li>
       @endif
-      @endif
+    @endif
   </ul>
 </div>
 @endsection
