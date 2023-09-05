517b0dfa6e1023590c3d921c24ba74f367c22dcb
Hai Tran <hai.ts.2166@aptechlearning.edu.vn>
2023-09-05 14:25:06 +0700

    remove obsolete code

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,15 +6,3 @@ import.meta.glob([
   '../fonts/**',
 ]);
 import * as bootstrap from 'bootstrap';
-
-const triggerTabList = document.querySelectorAll('#pills-tab button')
-triggerTabList.forEach(triggerEl => {
-  const tabTrigger = new bootstrap.Tab(triggerEl)
-
-  triggerEl.addEventListener('click', event => {
-    event.preventDefault()
-    const url = new URL(location.href)
-    url.searchParams.set('page', 1)
-    tabTrigger.show()
-  })
-})
